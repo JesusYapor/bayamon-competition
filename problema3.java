c0639fbd93ca6b0bddb6d325e9a1965b22765bd4
AlfreditoLaptop <AlfreditoLaptop>
2016-11-06 16:12:57 -0400

    fixed array and string structure

--- problema3.java
+++ problema3.java
@@ -3,18 +3,18 @@ import java.util.Scanner;
 public class problema3 {
 	public static void main(String[] args)
 	{
-	 Scanner input = new Scanner(System.in);
+	Scanner input = new Scanner(System.in);
 		
 		int n = 0;
 		int n2 = 0;
 		int math = 0;
 
 		
-		System.out.print("How many mathematical problem would you like to solve?" );
+		System.out.print("How many mathematical problem would you like to solve?\n" );
 		math = input.nextInt();
 		
 		for (int a = 1; a <= math; a++) {
-			System.out.print("What kind of mathematical problem would you like to solve? (ei. +,-,*,/)" );
+			System.out.print("What kind of mathematical problem would you like to solve? (ei. +,-,*,/)\n" );
 			String problem = input.next();
 			
 		
@@ -23,7 +23,7 @@ public class problema3 {
 				{
 					for (int c = 0; c < a1[r].length; c++)
 					{
-						System.out.print("Enter the numbers on row " + r + " column: " + c );
+						System.out.print("Enter the numbers on row " + r + " column " + c + ":\n");
 						n = input.nextInt();
                   a1[r][c] = n;
 								
@@ -37,15 +37,13 @@ public class problema3 {
 				{
 					for (int c = 0; c < a2[r].length; c++)
 					{
-						System.out.print("Enter the numbers on row " + r + " column: " + c);
+						System.out.print("Enter the numbers on row " + r + " column " + c + ":\n");
 						n2 = input.nextInt();
 						a2[r][c] = n2;
 						
 					}//for m
 					
 				}//for l
-				
-			
 				
 			if (problem.equals("+")) {
 				System.out.println("Suma");
@@ -54,14 +52,11 @@ public class problema3 {
 					for (int c = 0; c < 4; c++)
 					{
 						System.out.print(a1[r][c] + a2[r][c] + " \t");
-						
 					}//for c
-					
 					System.out.println();
 				}//for r
-
-			
 			}
+         
 			else if (problem.equals("-")) {
 				System.out.println("Resta");
 				for (int r = 0; r < 4; r++)
@@ -101,11 +96,7 @@ public class problema3 {
 					System.out.println();
 				}//for r
 			}
-			
-			
-			
 		}
-		input.close();
-		
+		input.close();		
 	}//main
 }//class
